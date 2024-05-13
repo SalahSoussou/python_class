5fa7e9f117b0d58a642867b544a69a856656a583
SalahSoussou <kaka79702@gmail.com>
2024-05-13 19:45:03 +0100

    ok

--- strings.py
+++ strings.py
@@ -4,11 +4,28 @@ b = 'string'
 
 print(a+b) #  hello string
 
-myStr = 'String"test"'
+myStr = 'String"test" end'
 
-print(myStr) #  String"test"
+print(myStr) #  String"test" end
 
 # Indexing (Access Item)
 
 print(myStr[0]) #  Index [0]=> S
+print(myStr[1]) #  Index [0]=> t
+print(myStr[-1]) #  Index [-1]=> d  start from the end
+
+# slicing ( Access Multiple Items )
+# [start:end]
+# [start:end:steps]
+
+myString = "I Love Python"
+
+print(myString[2:5]) # Lov
+print(myString[2:6]) # Love
+print(myString[:7]) # I Love
+print(myString[7:]) # Python
+
+
+
+
 
