7e2ef644d011857caaf211bef6a5c8dba4d52c52
SalahSoussou <soussousalahiddine@gmail.com>
2024-09-14 19:40:04 +0100

    lists

--- Numbers.py
+++ Numbers.py
@@ -14,3 +14,14 @@ print(complex(100))# (100+0j)
 print(10.5)  #10.5
 print(int(10.5)) #10
 print(complex(10.5)) # (10.5+0j)
+
+
+# [%] Modulus
+# [**] Exponent
+# [//] Floor Division
+
+print(100 // 20) # 5
+print(101 // 20) # 5
+print(101 % 20) # 1
+print(110 // 20) # 5
+print(120 // 20) # 6
