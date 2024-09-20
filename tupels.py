ab8753111456f2282b397b074ec10b5b5a52e13c
SalahSoussou <soussousalahiddine@gmail.com>
2024-09-20 20:32:54 +0100

    ok

--- tupels.py
+++ tupels.py
@@ -1,1 +1,17 @@
 
+myYuple = ("salah","soussou","python")
+
+myYupleTow = "salah","soussou","python"
+
+
+print(type(myYuple))  # <class 'tuple'>
+print(type(myYupleTow))  # <class 'tuple'>
+
+
+
+# Tuple indexing
+
+a = (1,2,3,4,5,6,7)
+
+print(a[3]) # 4
+print(a[3:]) # (4, 5, 6, 7)
